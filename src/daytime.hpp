3bd841bd7ad58281f90c5b81c2e5a40a1cfc3dc7
Xpol Wan <xpolife@gmail.com>
2016-09-18 18:23:28 +0800

    inline.

--- src/daytime.hpp
+++ src/daytime.hpp
@@ -1,7 +1,7 @@
 #ifndef __DAYTIME_HPP__
 #define __DAYTIME_HPP__
 
-std::string make_daytime_string()
+inline std::string make_daytime_string()
 {
   using namespace std;
   auto now = time(nullptr);
